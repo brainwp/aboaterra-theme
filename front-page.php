9f3bf9ec540ff018ee33e74aa0e415061067af82
matheus <contato@matheusgimenez.com.br>
2016-03-28 16:15:07 -0300

    fix header, mobile & issue #3

--- front-page.php
+++ front-page.php
@@ -7,5 +7,13 @@
  */
 
 get_header(); ?>
+	<section class="home-slider">
+		<?php $slider_name = 'Home Slider';?>
+		<?php $slider = get_page_by_title( $slider_name, OBJECT, 'brasa_slider_cpt' );?>
+		<?php if ( $slider && is_object( $slider ) && class_exists( 'Brasa_Slider' ) ) : ?>
+			<?php $brasa_slider = new Brasa_Slider();?>
+			<?php echo $brasa_slider->shortcode( array( 'name' => $slider_name, 'size' => 'full' ) );?>
+		<?php endif;?>
+	</section><!-- .col-md-12 home-slider -->
 <?php
 get_footer();
