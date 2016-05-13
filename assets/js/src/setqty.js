7540fa1bf852d8dbd6861da74fcd2cde3ad42306
matheus <contato@matheusgimenez.com.br>
2016-05-13 15:51:49 -0300

    issue #40

--- assets/js/src/setqty.js
+++ assets/js/src/setqty.js
@@ -3,8 +3,10 @@ jQuery(document).ready(function($) {
 	$( '.woocommerce .buttons-qty span').on( 'click', function(e){
 		var $input = $( this ).parent()[0];
 		var $input = $( $input ).children( 'input' );
+		if ( $( 'body' ).hasClass( 'woocommerce-cart' ) ) {
+			$input = $( $input[0] );
+		}
 		var value = parseInt( $input.val() );
-
 		if ( $( this ).html() == '+' ) {
 			var value = value + 1;
 		} else {
@@ -18,11 +20,18 @@ jQuery(document).ready(function($) {
 		$input.trigger( 'change' );
 	});
 	$( '.woocommerce .buttons-qty input').on( 'change', function(e){
+		if ( $( 'body' ).hasClass( 'woocommerce-cart' ) ) {
+			var selector = 'input[name="' + $( this ).attr( 'data-id' ) + '"]';
+			$( selector ).val( $( this ).val() );
+			return;
+		}
+
 		if ( $( this ).hasClass( 'single-product' ) ) {
 			$( 'input[name="quantity"]').val( $( this ).val() );
-		} else {
-			var $button_cart = $( '.woocommerce .products .post-' + $( this ).attr( 'data-id') + ' .ajax_add_to_cart' );
-			$button_cart.attr( 'data-quantity', $( this ).val() );
+			return;
 		}
+
+		var $button_cart = $( '.woocommerce .products .post-' + $( this ).attr( 'data-id') + ' .ajax_add_to_cart' );
+		$button_cart.attr( 'data-quantity', $( this ).val() );
 	});
 });
