0c6af9fbc065a26d8fb614ccf3de1e5d1af681a2
matheus <contato@matheusgimenez.com.br>
2016-05-20 16:18:43 -0300

    issue #41

--- assets/js/src/checkout-coupon.js
+++ assets/js/src/checkout-coupon.js
@@ -9,5 +9,6 @@ jQuery(document).ready(function($) {
 		$( 'form.checkout_coupon [name="coupon_code"]').val( $( 'form.checkout [name="coupon_code"]' ).val() );
 		$( 'html, body' ).animate({ scrollTop: $( '.woocommerce-info' ).top - 40 }, 300);
 		$( 'form.checkout_coupon' ).trigger( 'submit' );
+		$( 'form.checkout .shipping_method' ).trigger( 'change' );
 	});
 } );
