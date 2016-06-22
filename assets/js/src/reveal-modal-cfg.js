bd74954fa151ecd5219d67905bd9ffd198a01667
matheus <contato@matheusgimenez.com.br>
2016-06-22 16:28:47 -0300

    pagina de login/verificacao de cep em modal - #17

--- assets/js/src/reveal-modal-cfg.js
+++ assets/js/src/reveal-modal-cfg.js
@@ -7,4 +7,24 @@ jQuery(document).ready(function($) {
 			$content.removeClass( 'modal-bigger' );
 		}
 	});
+	$( 'body' ).on( 'click', '#reveal-modal-id a', function( e ){
+		var _href = $(this).attr('href');
+		var reveal_str = $( 'meta[name=reveal-modal-cfg-str]' ).attr('content');
+
+		if ( _href !== undefined && _href.lastIndexOf(reveal_str) != -1 ) {
+			e.preventDefault();
+			console.log( 'ahhhhhhuu' );
+			$( '#reveal-modal-id' ).find( '.close-reveal-modal').trigger( 'click' );
+			$.get( _href + '?reveal-modal-ajax=true', {}, function( response ){
+				var html = '<a class="close-reveal-modal">&#215;</a>' + response;
+				$( '#reveal-modal-id' ).html( html );
+				 setTimeout( function(){
+				 	$( '#reveal-modal-id' ).foundation('reveal', 'open');
+				 }, 500 );
+			});
+		} else {
+			e.preventDefault();
+			console.log( 'ooooooooooooooH NAO!');
+		}
+	} );
 });
