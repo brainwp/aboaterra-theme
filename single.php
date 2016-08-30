57d61884e4984bda853defa7ee87a3186172d601
Isaque Melo <isaquegbmelo2@gmail.com>
2016-08-30 15:44:27 -0300

    inicio ao layout do blog #48

--- single.php
+++ single.php
@@ -7,7 +7,7 @@
  */
 
 get_header(); ?>
-
+<div class="container">
 	<div id="primary" class="<?php echo odin_classes_page_sidebar(); ?>">
 		<main id="main-content" class="site-main" role="main">
 			<?php
@@ -29,7 +29,8 @@ get_header(); ?>
 			?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
 <?php
 get_sidebar();
+echo '</div>';
+
 get_footer();
