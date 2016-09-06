c3e23f7505410e0b42adb15ea7c12226bf392f56
Isaque Melo <isaquegbmelo2@gmail.com>
2016-09-06 20:29:33 -0300

    #24

--- assets/js/src/header.js
+++ assets/js/src/header.js
@@ -1,11 +1,1 @@
-jQuery(document).ready(function($) {
-	$('.menu-item-has-children').hover(function() {
-		$('.menu-item-has-children ul').css({
-			display: 'block',
-		});
-	}, function() {
-		$('.menu-item-has-children ul').css({
-			display: 'none',
-		});
-	});
-});
+
