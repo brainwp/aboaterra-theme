0c3f447096028d8d5abac57f49fb1d0046f3bccc
matheusgimenez <contato@matheusgimenez.com.br>
2016-09-29 20:19:49 -0300

    redes sociais & responsivo - issue #35

--- content-page.php
+++ content-page.php
@@ -7,7 +7,7 @@
  */
 $entry_content_class = '';
 if ( is_page_template( 'page-contato.php' ) ) {
-	$entry_content_class = 'col-md-5 pull-left';
+	$entry_content_class = 'col-md-5 pull-left col-xs-12';
 }
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -28,5 +28,22 @@ if ( is_page_template( 'page-contato.php' ) ) {
 		<div class="col-md-6 pull-right right-content">
 			<?php echo apply_filters( 'the_content', $value );?>
 		</div><!-- .col-md-6 pull-right right-content -->
+	<?php endif;?>
+	<?php if ( is_page_template( 'page-contato.php' ) ) : ?>
+	<div class="entry-content acompanhe">
+		<h1 class="entry-title">
+			<?php _e( 'Quer acompanhar nosso dia-dia aqui na roça? Siga nosso instagram e facebook!', 'odin' );?>
+		</h1><!-- .entry-title -->
+		<?php if ( $value = get_theme_mod( 'facebook', false ) ) : ?>
+			<a href="<?php echo esc_url( $value );?>" class="rounded-icon">
+				<i class="fa fa-facebook"></i>
+			</a>
+		<?php endif;?>
+		<?php if ( $value = get_theme_mod( 'instagram', false ) ) : ?>
+			<a href="<?php echo esc_url( $value );?>" class="rounded-icon">
+				<i class="fa fa-instagram"></i>
+			</a>
+		<?php endif;?>
+	</div>
 	<?php endif;?>
 </article><!-- #post-## -->
