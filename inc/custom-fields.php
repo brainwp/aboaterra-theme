dae1d75f8d4560a17160eaa66634ce67e76776c1
matheusgimenez <contato@matheusgimenez.com.br>
2016-09-29 18:56:31 -0300

    pagina contato - issue #35

--- inc/custom-fields.php
+++ inc/custom-fields.php
@@ -43,3 +43,39 @@ if(function_exists("register_field_group"))
 	));
 }
 
+if(function_exists("register_field_group"))
+{
+	register_field_group(array (
+		'id' => 'acf_conteudo-no-lado-direito',
+		'title' => 'Conteúdo no lado direito',
+		'fields' => array (
+			array (
+				'key' => 'field_57ed7e205e297',
+				'label' => 'Conteúdo no lado direito',
+				'name' => 'right_content',
+				'type' => 'wysiwyg',
+				'default_value' => '',
+				'toolbar' => 'full',
+				'media_upload' => 'yes',
+			),
+		),
+		'location' => array (
+			array (
+				array (
+					'param' => 'page_template',
+					'operator' => '==',
+					'value' => 'page-contato.php',
+					'order_no' => 0,
+					'group_no' => 0,
+				),
+			),
+		),
+		'options' => array (
+			'position' => 'normal',
+			'layout' => 'no_box',
+			'hide_on_screen' => array (
+			),
+		),
+		'menu_order' => 0,
+	));
+}
