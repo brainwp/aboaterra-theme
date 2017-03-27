d42ba813e59ca8668a883b1bb8f034c9c0e1c328
matheusgimenez <contato@matheusgimenez.com.br>
2017-03-27 17:19:59 -0300

    issue #57

--- assets/js/src/cart.js
+++ assets/js/src/cart.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function($) {
 	/**
 	 * Box de confirmação antes de deletar todo o cart
 	*/
-	$( '#cart-empty-link' ).on( 'click', function( e ) {
+	$( document ).on( 'click', '#cart-empty-link', function( e ) {
 		if ( ! confirm( $( this ).attr( 'data-confirm' ) ) ) {
 			e.preventDefault();
 		}
