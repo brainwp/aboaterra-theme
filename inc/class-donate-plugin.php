bb102bdba2b338f89754691d2017acb44318eea8
willowmagrini <brmagrini@gmail.com>
2019-01-31 13:48:35 -0200

    leva a doação para a pagina do carrinho

--- inc/class-donate-plugin.php
+++ inc/class-donate-plugin.php
@@ -21,6 +21,10 @@
 			add_action( 'init', array( $this,'cpt_instituicoes'), 0 );
 			// add custom fields
 			add_action('init', array( $this,'my_acf_add_local_field_groups'));
+			// add donation field to checkout
+			add_action( 'woocommerce_after_cart_table', array( $this,'donation_checkout_field' ));
+
+
 		}
 
 		/**
@@ -36,6 +40,34 @@
 
 			return self::$instance;
 		}
+		// Add donation field to checkout
+		public function donation_checkout_field( $checkout ) {
+			?>
+			<div class="donate_button_div col-sm-6">
+				<h2>Faça uma doação</h2>
+				<p>A cada compra você pode doar 1 real para colaborar com uma das instituições abaixo</p>
+				<?php
+				$loop = new WP_Query( array(
+				    'post_type' => 'instituicoes',
+				    'posts_per_page' => -1
+				  )
+				);
+				?>
+
+				<?php while ( $loop->have_posts() ) : $loop->the_post();
+					?>
+					<div class="col-md-4">
+
+						<input type="radio" name="imagem" id="<?php echo get_the_id() ?>" />
+						<label for="<?php echo get_the_id(); ?>"><img src="<?php echo get_the_post_thumbnail_url( )  ;?>" alt=""></label>
+						<h4><?php echo get_the_title( ); ?></h4>
+					</div>
+
+
+				<?php endwhile; wp_reset_query(); ?>
+			</div>
+			<?php
+		}
 		// Register Custom Post Type instituições
 		public function cpt_instituicoes() {
 			$labels = array(
@@ -46,23 +78,23 @@
 				'archives'              => __( 'Instituições', 'odin' ),
 				'attributes'            => __( 'Item Attributes', 'odin' ),
 				'parent_item_colon'     => __( 'Parent Item:', 'odin' ),
-				'all_items'             => __( 'All Items', 'odin' ),
-				'add_new_item'          => __( 'Add New Item', 'odin' ),
-				'add_new'               => __( 'Add New', 'odin' ),
-				'new_item'              => __( 'New Item', 'odin' ),
-				'edit_item'             => __( 'Edit Item', 'odin' ),
-				'update_item'           => __( 'Update Item', 'odin' ),
-				'view_item'             => __( 'View Item', 'odin' ),
-				'view_items'            => __( 'View Items', 'odin' ),
+				'all_items'             => __( 'Todas instituições', 'odin' ),
+				'add_new_item'          => __( 'Adicionar', 'odin' ),
+				'add_new'               => __( 'Adicionar', 'odin' ),
+				'new_item'              => __( 'Nova', 'odin' ),
+				'edit_item'             => __( 'Editar', 'odin' ),
+				'update_item'           => __( 'Atualizar', 'odin' ),
+				'view_item'             => __( 'Ver item', 'odin' ),
+				'view_items'            => __( 'Ver itens', 'odin' ),
 				'search_items'          => __( 'Search Item', 'odin' ),
 				'not_found'             => __( 'Not found', 'odin' ),
 				'not_found_in_trash'    => __( 'Not found in Trash', 'odin' ),
-				'featured_image'        => __( 'Featured Image', 'odin' ),
-				'set_featured_image'    => __( 'Set featured image', 'odin' ),
-				'remove_featured_image' => __( 'Remove featured image', 'odin' ),
-				'use_featured_image'    => __( 'Use as featured image', 'odin' ),
-				'insert_into_item'      => __( 'Insert into item', 'odin' ),
-				'uploaded_to_this_item' => __( 'Uploaded to this item', 'odin' ),
+				'featured_image'        => __( 'Imagem destacada', 'odin' ),
+				'set_featured_image'    => __( 'Definir imagem', 'odin' ),
+				'remove_featured_image' => __( 'Remover imagem', 'odin' ),
+				'use_featured_image'    => __( 'Usar como imagem', 'odin' ),
+				'insert_into_item'      => __( 'Inserir no item', 'odin' ),
+				'uploaded_to_this_item' => __( 'Upload', 'odin' ),
 				'items_list'            => __( 'Items list', 'odin' ),
 				'items_list_navigation' => __( 'Items list navigation', 'odin' ),
 				'filter_items_list'     => __( 'Filter items list', 'odin' ),
@@ -71,7 +103,7 @@
 				'label'                 => __( 'Instituição', 'odin' ),
 				'description'           => __( 'instituições para doação', 'odin' ),
 				'labels'                => $labels,
-				'supports'              => array( 'title', 'editor' ),
+				'supports'              => array( 'title', 'editor', 'thumbnail' ),
 				'hierarchical'          => false,
 				'public'                => true,
 				'show_ui'               => true,
