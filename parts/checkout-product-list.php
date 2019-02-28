f6f768ad706af78b063ce17b294daf5b865668cf
willowmagrini <brmagrini@gmail.com>
2019-02-28 15:14:11 -0300

    css e php para diminuir foto de produtos no checkout

--- parts/checkout-product-list.php
+++ parts/checkout-product-list.php
@@ -15,15 +15,15 @@ do_action( 'woocommerce_review_order_before_cart_contents' );
 			if ( $_product && $_product->exists() && $cart_item['quantity'] > 0 && apply_filters( 'woocommerce_checkout_cart_item_visible', true, $cart_item, $cart_item_key ) ) {
 					?>
 				<a class="each-product-checkout" href="<?php echo get_permalink( $_product->id );?>">
-					<div class="col-md-2 thumbnail">
+					<div class="col-xs-2 thumbnail">
 						<?php echo get_the_post_thumbnail( $_product->id );?>
 					</div><!-- .col-md-2 -->
-					<div class="col-md-6 name">
+					<div class="col-xs-6 name">
 						<?php echo apply_filters( 'woocommerce_cart_item_name', $_product->get_title(), $cart_item, $cart_item_key ) . '&nbsp;'; ?>
 						<?php echo apply_filters( 'woocommerce_checkout_cart_item_quantity', ' <strong class="product-quantity">' . sprintf( '&times; %s', $cart_item['quantity'] ) . '</strong>', $cart_item, $cart_item_key ); ?>
 						<?php echo WC()->cart->get_item_data( $cart_item ); ?>
 					</div><!-- .col-md-6 -->
-					<div class="col-md-4 price">
+					<div class="col-xs-4 price">
 						<?php echo apply_filters( 'woocommerce_cart_item_subtotal', WC()->cart->get_product_subtotal( $_product, $cart_item['quantity'] ), $cart_item, $cart_item_key ); ?>
 					</div><!-- .col-md-12 price -->
 				</a>
